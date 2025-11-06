13ba77764dc785176f62b555d4101ac11dac140b
11249a366-smk <11249a366@kanchiuniv.ac.in>
2025-11-05 21:27:19 -0800

    Refactor student class and update input/output methods

--- student.cpp
+++ student.cpp
@@ -2,47 +2,47 @@
 using namespace std;
 class student{
     public:
-    char sname[30];
-    char sreg[10];
-    int syear;
-    char sdept[25];
-    int subjects;
-    int marks[10];
-    int total=0;
-    int avg=0;
-    void get(){
-        cout<<"enter the name of the student:";
-        cin>>sname;
-        cout<<"enter your register number:";
-        cin>>sreg;
-        cout<<"currently which year are you pursuing in your university:";
-        cin>>syear;
-        cout<<"which department have you choosen:";
-        cin>>sdept;
-        cout<<"how many subjects are you studying now:";
-        cin>>subjects;
-        
-    }
-    void details(){
-        for(int i=1;i<=subjects;i++){
-            cout<<"enter the marks of the subject:"<<i;
+    char name[25],dept[5],reg[10];
+    float total,avg,marks[5];
+    void read(){
+        cout<<"enter your name"<<endl;
+        cin>>name;
+        cout<<"enter your dept"<<endl;
+        cin>>dept;
+        cout<<"enter your register number"<<endl;
+        cin>>reg;
+        cout<<"enter marks";
+        for(int i=1;i<=5;i++){
             cin>>marks[i];
             total+=marks[i];
         }
-            avg=total/subjects;
-
+        avg=total/5;
     }
     void display(){
-        cout<<"the name of the student is"<<sname<<endl;
-        cout<<"the register number of the student is:"<<sreg<<endl;
-        cout<<"the total marks are:"<<total<<endl;
-        cout<<"the average is:"<<avg<<endl;
-
+        cout<<"NAME:"<<name<<endl;
+        cout<<"DEPARTMENT:"<<dept<<endl;
+        cout<<"register_no.:"<<reg<<endl;
+        for(int i=1;i<=5;i++){
+            cout<<marks[i]<<endl;
+        }
+        cout<<"total:"<<total<<endl;
+        cout<<"avg:"<<avg<<endl;
     }
+
 };
 int main(){
-    student sample;
-    sample.get();
-    sample.details();
-    sample.display();
+    int n,i=1;
+cout<<"how many students detailes you want to enter"<<endl;
+cin>>n; 
+student s[10];
+for(int i=0;i<n;i++){
+    cout<<"enter"<<i+1<<"student detailes"<<endl;
+    s[i].read();
+    }
+for(int i=0;i<n;i++){
+    cout<<"detailes of "<<i+1<<"student"<<endl;
+    s[i].display();
+}
+cout<<"program over";
+return 0;
 }
